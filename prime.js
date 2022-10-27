535e18e1a555d7be5a9c70c6e4d3a7514cc78b36
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-10-27 13:21:31 +0200

    ✨ prime numbers

---
+++ prime.js
@@ -0,0 +1,30 @@
+/*
+ * Reto #3
+ * ¿ES UN NÚMERO PRIMO?
+ * Fecha publicación enunciado: 17/01/22
+ * Fecha publicación resolución: 24/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+for (i = 2; i <= 100; i++) {
+  k = 0;
+  p = i;
+  for (j = i; j > 0; j--) {
+    if (p % j == 0) {
+      k++;
+    }
+  }
+  if (k == 2) {
+    console.log(i);
+  }
+}
